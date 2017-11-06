6b034aabbed23767c9a05ac5cfeeadeffd3cd508
PJ Frias <peejfrias@gmail.com>
2017-11-06 23:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function flipTails () {
 function tailsNeverFails(){
   const countTails = 0
   while (flipTails()) {
-    return ++countTails
-  }  
+    countTails += 1
+    return counTails
+  }
 }
