5617929f63cc3059cca09923c121ad534cddb5da
PJ Frias <peejfrias@gmail.com>
2017-11-06 23:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function printBadges(array) {
   for (let i = 0; i < array.length ; i++) {
     console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
-    return array
+    return ${array}
   }
 }
